5521aa492a2ffb6bb1cf0ef7fe96f67ff3d399eb
ManeeshWije <mkandage@uoguelph.ca>
2023-09-14 00:11:14 -0400

    modify regex to include course name, handle edge cases when course names span multiple lines, use file1 to reduce headaches

--- parser/main.py
+++ parser/main.py
@@ -2,18 +2,17 @@ import re
 
 
 class Course:
-    # TODO: define property 'name'
-    def __init__(self, code, prerequisites):
+    def __init__(self, code, name, prerequisites):
         self.code = code
-        # self.name = name
+        self.name = name
         self.prerequisites = prerequisites
 
-    # TODO: add property 'name'
     def __str__(self):
-        return f"Course Code: {self.code}\nPrerequisites: {self.prerequisites}"
+        return f"Course Code: {self.code}\nCourse Name: {self.name}\nPrerequisites: {self.prerequisites}"
 
 
-with open("../f23_courses2.txt", "r", encoding="ISO-8859-1") as file:
+# Use the correct encoding so out program doesn't crash and read char by char making sure to remove newlines and spaces
+with open("../f23_courses1.txt", "r", encoding="ISO-8859-1") as file:
     file_contents = file.read()
     file_contents.strip()
 
@@ -25,20 +24,28 @@ courses = []
 # Split the text into course blobs using "Location(s):" as the delimiter since every course has a location at the end
 course_blobs = re.split(r"Location\(s\):", start)
 
-# Define a regular expression pattern to match course codes
-# TODO: Try to also capture the course name. This does the code only, the name is right next to the code but need to figure out when to stop reading it
-course_pattern = r"([A-Z]{3,4}\*\d{4})"
+# Define a regular expression pattern to match course codes and course name combined and prereq seperately
+course_pattern = r"([A-Z]{3,4}\*\d{4})\s+(.*?)\s+\[[\d.]+\]"
 prerequisite_pattern = r"Prerequisite\(s\):(.*?)(Restriction\(s\)|Location\(s\)|Co-requisite\(s\)|Equate\(s\)|Offering\(s\)|Department\(s\))"
 
 # Loop through each course blob and extract the first course code
 for blob in course_blobs:
-    course_codes = re.findall(course_pattern, blob)
-    # Use re.findall with re.DOTALL to find all prerequisites in the text
+  blob.strip()
+  # DOTALL flag will allow us to span multiple lines
+  matches = re.search(course_pattern, blob, re.DOTALL)
+  if matches:
+    course_code = matches.group(1)
+    course_name = matches.group(2)
+    # Since some course names span two lines and contain other useless info, we split on double space so we can take the first element in that array
+    course_name = course_name.split("  ")[0].strip()
     prerequisites = re.findall(prerequisite_pattern, blob, re.DOTALL)
-    if course_codes and not prerequisites:
-        courses.append(Course(course_codes[0], []))
-    elif course_codes and prerequisites:
-        courses.append(Course(course_codes[0], prerequisites[0][0].strip()))
+    # Since some courses do not have prereq, handle that case seperately
+    if course_code and course_name and not prerequisites:
+      courses.append(Course(course_code, course_name, []))
+    elif course_code and course_name and prerequisites:
+      courses.append(
+          Course(course_code, course_name, prerequisites[0][0].strip()))
 
 for course in courses:
-    print(course)
+  print(course)
+  print('\n')
