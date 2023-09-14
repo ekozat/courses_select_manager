d3a6f95c584dfe4ef2643c807fb09f88d7898a1f
Fee Kim Ah-Poa <fahpoa@uoguelph.ca>
2023-09-14 18:32:13 -0400

    Done with subject and code

--- searcher/search.py
+++ searcher/search.py
@@ -39,13 +39,90 @@ def name_search():
 
     print("\n") #maybe we should clear instead of newline?
 
+
 def subject_search():
-    #TO DO
+# Get the subject abbreviation from user
+    subject_name = input("Enter the Course Name: ")
+    sub_upper = subject_name.upper()
+
+
+    # Read the data from the CSV file
+    with open(filepath, mode='r', newline='', encoding='utf-8') as file:
+        fieldnames = ["course code", "course name", "prerequisites"]
+        reader = csv.DictReader(file, fieldnames=fieldnames)
+
+        # Initialize a list to store matching rows
+        matching_rows = []
+        
+        # Iterate through the rows in the CSV file
+        for row in reader:
+
+            # Ensure that 'course code' is a valid column name in your CSV file
+            if 'course code' in row:
+                course_code = row['course code']
+                
+                # Check if the course code starts with the given subject
+                if course_code.startswith(sub_upper):
+                    matching_rows.append(row)
+
+        if not matching_rows:
+            print("Subject doesn't exist")
+        else:
+            # matching_rows contains the rows that needs to be output
+
+            for row in matching_rows:
+                print(row)
+
+
+    file.close()
+
     print("\n")
 
+
 def code_search():
-    #TO DO
+# Input text to search for (e.g., "CIS 1300" or "CIS1300" or "CIS*1300")
+    search_text = input("Enter a course code to search for: ").replace(" ", "").replace("*", "")
+    search_text_Upp = search_text.upper()
+
+    with open(filepath, mode='r', newline='', encoding='utf-8') as file:
+        fieldnames = ["course code", "course name", "prerequisites"]
+        reader = csv.DictReader(file, fieldnames=fieldnames)
+        
+        # Initialize a list to store matching rows
+        matching_rows = []
+        
+        # Iterate through the rows in the CSV file
+        for row in reader:
+            # Extract the course code and remove spaces and asterisks
+            course_code = row['course code'].replace(" ", "").replace("*", "")
+            
+            # Check if the course code contains the search text
+            if search_text_Upp in course_code:
+                matching_rows.append(row)
+
+        if not matching_rows:
+            print("No matching rows found.")
+        else:
+            # Display the matching rows
+            for row in matching_rows:
+                #print (row)
+                print("\n")
+                print("-------------------------------------------------------------------------------------------------")
+                print("\n")
+                print("Course Code:", row['course code'])
+                print("\n")
+                print("Course Name:", row['course name'])
+                print("\n")
+                print("Prerequisites:", row['prerequisites'])
+                print("\n")
+                print("-------------------------------------------------------------------------------------------------")
+                print()
+    file.close()
+    
     print("\n")
+
+
+
 
 while True:
     print("Welcome to the prerequisite searcher! \n\nPlease type in the number of the search would you like to execute.")
