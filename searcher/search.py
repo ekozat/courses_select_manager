93840fd7b401533c0b0691b35b3af54d6377e3ac
ekozatch <ekozatch@uoguelph.ca>
2023-09-14 14:03:01 -0400

    cli loop implement

--- searcher/search.py
+++ searcher/search.py
@@ -3,6 +3,34 @@ import csv
 
 csv_file = "parsed_courses.csv"
 
+def course_search():
+    print("\n")
+
+def subject_search():
+    print("\n")
+
+def code_search():
+    print("\n")
+
+while True:
+    print("Welcome to the prerequisite searcher! \n\nPlease type in the number of the search would you like to execute.")
+    print("1. Course Specific Search")
+    print("2. Subject Search")
+    print("3. Course Code Search")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == '1': course_search()
+    elif choice == '2': subject_search()
+    elif choice == '3': option3()
+    elif choice == '4':
+        print("Thank you for searching.")
+        break
+    else:
+        print("Invalid choice. Please select a valid option.")
+
+
 # Discuss with the groupt to change export of the file to diff dir
 parentpath = path.abspath(path.dirname(path.dirname(__file__)))
 filepath = path.join(parentpath, "parser", csv_file)
@@ -14,6 +42,7 @@ with open(filepath, mode='r', newline='', encoding='utf-8') as file:
 
     # iteration example
     for row in reader:
+        print(row)
         print(reader.line_num)
 
     
