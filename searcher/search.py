0f4e7a7774a1419b2b94aa27d11a021cc3ef70f2
ekozatch <ekozatch@uoguelph.ca>
2023-09-14 16:56:55 -0400

    name search finish

--- searcher/search.py
+++ searcher/search.py
@@ -7,7 +7,7 @@ csv_file = "parsed_courses.csv"
 parentpath = path.abspath(path.dirname(path.dirname(__file__)))
 filepath = path.join(parentpath, "parser", csv_file)
 
-def course_search():
+def name_search():
     # Input course they want to find
     course_name = input("Enter the course name: ")
 
@@ -15,34 +15,48 @@ def course_search():
     with open(filepath, mode='r', newline='', encoding='utf-8') as file:
         fieldnames = ["course code", "course name", "prerequisites"]
         reader = csv.DictReader(file, fieldnames=fieldnames)
+        found = False
 
-        # iteration example
+        # search iteration
         for row in reader:
+            # consider match
             if row['course name'].casefold() == course_name.casefold():
-                print(row)
+
+                # table formatting
+                print("\n{:^30}|{:^30}|{:^30}".format("Course Code", "Course Name", "Prerequisites"))
+                for x in range(95): 
+                    print("-", end="")
+                print("\n{:^30}|{:^30}|{:^30}".format(row['course code'], row['course name'], row['prerequisites']))
+
+                found = True
                 break
-            # print(reader.line_num)
+        
+        # match missing
+        if found is False:
+            print("Match not found.")
 
     file.close()
 
     print("\n") #maybe we should clear instead of newline?
 
 def subject_search():
+    #TO DO
     print("\n")
 
 def code_search():
+    #TO DO
     print("\n")
 
 while True:
     print("Welcome to the prerequisite searcher! \n\nPlease type in the number of the search would you like to execute.")
-    print("1. Course Specific Search")
+    print("1. Course Name Search")
     print("2. Subject Search")
     print("3. Course Code Search")
     print("4. Exit")
     
     choice = input("Enter your choice: ")
     
-    if choice == '1': course_search()
+    if choice == '1': name_search()
     elif choice == '2': subject_search()
     elif choice == '3': code_search()
     elif choice == '4':
