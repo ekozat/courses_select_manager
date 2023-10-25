94b31643ff55d6434637d717fce8a3e48babb9d7
Maneesh Kandage Wijewardhana <mkandage@uoguelph.ca>
2023-10-25 18:22:17 -0400

    add logical and and logical or to getPreq and getRestrict

--- sprint5/sprint5-files/html/get_courses_by_prereq.php
+++ sprint5/sprint5-files/html/get_courses_by_prereq.php
@@ -1,7 +1,20 @@
 <?php
+
 header("Content-Type: application/json");
 
 require 'db_connection.php';
+
+function check_valid($conn, $course_code, $course_num)
+{
+    if (isset($course_num) && (strlen($course_num) == 3 || strlen($course_num) == 4 || ($course_num == "" && $course_code == ""))) {
+        return true;
+    } else {
+        http_response_code(404);
+        echo json_encode(array('error' => 'Malformed prerequisite array'));
+        close_con($conn);
+        return false;
+    }
+}
 
 $conn = open_con();
 
@@ -13,26 +26,66 @@ if (!$conn) {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             if (isset($_GET['prerequisites'])) {
                 $prerequisites = $_GET['prerequisites'];
-                $prerequisitesArray = explode(',', $prerequisites);
+                if (str_contains($prerequisites, "|")) {
+                    $prerequisitesArray = explode('|', $prerequisites);
+                    $data = array();
 
-                $data = array();
+                    foreach ($prerequisitesArray as $prerequisite) {
+                        // Check if the prerequisite has 3 or 4 numbers after the letters
+                        $parts = explode('*', $prerequisite);
+                        $courseCode = $parts[0];
+                        $courseNumber = $parts[1];
+                        if (check_valid($conn, $courseCode, $courseNumber)) {
+                            $escapedPrerequisite = mysqli_real_escape_string($conn, trim($prerequisite));
+                            $sql = "SELECT * FROM coursesDB WHERE prerequisites LIKE '%$escapedPrerequisite%'";
 
-                foreach ($prerequisitesArray as $prerequisite) {
-                    // Check if the prerequisite has 3 or 4 numbers after the letters
-                    $parts = explode('*', $prerequisite);
-                    $courseCode = $parts[0];
-                    $courseNumber = $parts[1];
+                            $result = $conn->query($sql);
 
-                    if (isset($courseNumber) && (strlen($courseNumber) == 3 || strlen($courseNumber) == 4 || ($courseNumber == "" && $courseCode == ""))) {
-                        $escapedPrerequisite = mysqli_real_escape_string($conn, trim($prerequisite));
-                    } else {
+                            if ($result) {
+                                while ($row = $result->fetch_assoc()) {
+                                    // Remove quotation marks from the values
+                                    foreach ($row as $key => $value) {
+                                        $row[$key] = str_replace('"', '', $value);
+                                    }
+                                    $data[] = $row;
+                                }
+                            }
+                        } else {
+                            return;
+                        }
+                    }
+                    if (empty($data)) {
                         http_response_code(404);
-                        echo json_encode(array('error' => 'Malformed prerequisite array'));
-                        close_con();
-                        return;
+                        echo json_encode(array('error' => 'No matching prerequisites found'));
+                    } else {
+                        echo json_encode($data);
+                    }
+                } else {
+                    $prerequisitesArray = explode(',', $prerequisites);
+                    $data = array();
+
+                    $condition = array();
+                    $combined  = "";
+
+                    foreach ($prerequisitesArray as $prerequisite) {
+                        // Check if the prerequisite has 3 or 4 numbers after the letters
+                        $parts = explode('*', $prerequisite);
+                        $courseCode = $parts[0];
+                        $courseNumber = $parts[1];
+
+                        if (check_valid($conn, $courseCode, $courseNumber)) {
+                            $escapedPrerequisite = mysqli_real_escape_string($conn, trim($prerequisite));
+                            // Build each AND case
+                            $condition[] = "prerequisites LIKE '%$escapedPrerequisite%'";
+                        } else {
+                            return;
+                        }
+
+                        // Combine all conditions with AND
+                        $combined = implode(' AND ', $condition);
                     }
 
-                    $sql = "SELECT * FROM coursesDB WHERE prerequisites LIKE '%$escapedPrerequisite%'";
+                    $sql = "SELECT * FROM coursesDB WHERE $combined";
 
                     $result = $conn->query($sql);
 
@@ -45,14 +98,15 @@ if (!$conn) {
                             $data[] = $row;
                         }
                     }
+
+                    if (empty($data)) {
+                        http_response_code(404);
+                        echo json_encode(array('error' => 'No matching prerequisites found'));
+                    } else {
+                        echo json_encode($data);
+                    }
                 }
 
-                if (empty($data)) {
-                    http_response_code(404);
-                    echo json_encode(array('error' => 'No matching prerequisites found'));
-                } else {
-                    echo json_encode($data);
-                }
             } else {
                 $data = array();
                 $sql = "SELECT * FROM coursesDB WHERE prerequisites = '\"()\"' ";
@@ -79,4 +133,4 @@ if (!$conn) {
     }
     close_con($conn);
 }
-?>
+
--- sprint5/sprint5-files/html/get_courses_by_restrict.php
+++ sprint5/sprint5-files/html/get_courses_by_restrict.php
@@ -1,7 +1,20 @@
 <?php
+
 header("Content-Type: application/json");
 
 require 'db_connection.php';
+
+function check_valid($conn, $course_code, $course_num)
+{
+    if (isset($course_num) && (strlen($course_num) == 3 || strlen($course_num) == 4 || ($course_num == "" && $course_code == ""))) {
+        return true;
+    } else {
+        http_response_code(404);
+        echo json_encode(array('error' => 'Malformed prerequisite array'));
+        close_con($conn);
+        return false;
+    }
+}
 
 $conn = open_con();
 
@@ -13,26 +26,69 @@ if (!$conn) {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             if (isset($_GET['restrictions'])) {
                 $restrictions = $_GET['restrictions'];
-                $restrictionsArray = explode(',', $restrictions);
+                if (str_contains($restrictions, "|")) {
+                    $restrictionsArray = explode('|', $restrictions);
 
-                $data = array();
+                    $data = array();
 
-                foreach ($restrictionsArray as $restriction) {
-                    // Check if the restriction has 3 or 4 numbers after the letters
-                    $parts = explode('*', $restriction);
-                    $courseCode = $parts[0];
-                    $courseNumber = $parts[1];
+                    foreach ($restrictionsArray as $restriction) {
+                        // Check if the restriction has 3 or 4 numbers after the letters
+                        $parts = explode('*', $restriction);
+                        $courseCode = $parts[0];
+                        $courseNumber = $parts[1];
 
-                    if (isset($courseNumber) && (strlen($courseNumber) == 3 || strlen($courseNumber) == 4 || ($courseNumber == "" && $courseCode == ""))) {
-                        $escapedRestriction = mysqli_real_escape_string($conn, trim($restriction));
-                    } else {
+
+                        if (check_valid($conn, $courseCode, $courseNumber)) {
+                            $escapedRestriction = mysqli_real_escape_string($conn, trim($restriction));
+                            $sql = "SELECT * FROM coursesDB WHERE restrictions LIKE '%$escapedRestriction%'";
+
+                            $result = $conn->query($sql);
+
+                            if ($result) {
+                                while ($row = $result->fetch_assoc()) {
+                                    // Remove quotation marks from the values
+                                    foreach ($row as $key => $value) {
+                                        $row[$key] = str_replace('"', '', $value);
+                                    }
+                                    $data[] = $row;
+                                }
+                            }
+                        } else {
+                            return;
+                        }
+                    }
+                    if (empty($data)) {
                         http_response_code(404);
-                        echo json_encode(array('error' => 'Malformed restrictions array'));
-                        close_con();
-                        return;
+                        echo json_encode(array('error' => 'No matching restrictions found'));
+                    } else {
+                        echo json_encode($data);
+                    }
+                } else {
+                    $restrictionsArray = explode(',', $restrictions);
+                    $data = array();
+
+                    $condition = array();
+                    $combined  = "";
+
+                    foreach ($restrictionsArray as $restriction) {
+                        // Check if the prerequisite has 3 or 4 numbers after the letters
+                        $parts = explode('*', $restriction);
+                        $courseCode = $parts[0];
+                        $courseNumber = $parts[1];
+
+                        if (check_valid($conn, $courseCode, $courseNumber)) {
+                            $escapedRestriction = mysqli_real_escape_string($conn, trim($restriction));
+                            // Build each AND case
+                            $condition[] = "restrictions LIKE '%$escapedRestriction%'";
+                        } else {
+                            return;
+                        }
+
+                        // Combine all conditions with AND
+                        $combined = implode(' AND ', $condition);
                     }
 
-                    $sql = "SELECT * FROM coursesDB WHERE restrictions LIKE '%$escapedRestriction%'";
+                    $sql = "SELECT * FROM coursesDB WHERE $combined";
 
                     $result = $conn->query($sql);
 
@@ -45,13 +101,14 @@ if (!$conn) {
                             $data[] = $row;
                         }
                     }
-                }
 
-                if (empty($data)) {
-                    http_response_code(404);
-                    echo json_encode(array('error' => 'No matching restrictions found'));
-                } else {
-                    echo json_encode($data);
+                    if (empty($data)) {
+                        http_response_code(404);
+                        echo json_encode(array('error' => 'No matching restrictions found'));
+                    } else {
+                        echo json_encode($data);
+                    }
+
                 }
             } else {
                 $data = array();
@@ -79,4 +136,4 @@ if (!$conn) {
     }
     close_con($conn);
 }
-?>
+
