0f276cc04dbe3787b71912fbf9100453232f99e9
Maneesh Kandage Wijewardhana <mkandage@uoguelph.ca>
2023-11-01 12:53:39 -0400

    Adding changes to homepage

---
+++ sprint5/sprint5-files/html/course_generator/index.php
@@ -0,0 +1,27 @@
+<?php $pageTitle="Course Generator Page"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Merriweather:wght@300&display=swap" rel="stylesheet">
+
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+
+    <main>
+        <h1> Hi. This page is still under construction - group 101<h1>
+    </main>
+
+</body>
+</html>
