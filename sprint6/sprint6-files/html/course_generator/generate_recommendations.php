0cacf4394823757880c373a49ebf16588a1bcc95
Maneesh Kandage Wijewardhana <mkandage@uoguelph.ca>
2023-11-04 21:49:10 -0400

    Recommendatons added

---
+++ sprint6/sprint6-files/html/course_generator/generate_recommendations.php
@@ -0,0 +1,63 @@
+<?php
+header('Content-Type: application/json');
+ 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the entered courses from the POST request and decode it
+    $enteredCoursesJSON = isset($_POST['enteredCourses']) ? $_POST['enteredCourses'] : '[]';
+ 
+    $enteredCourses = json_decode($enteredCoursesJSON, true);
+ 
+    if ($enteredCourses === null) {
+        // Handle the case where enteredCourses is not valid JSON
+        http_response_code(400); // Bad Request
+        echo "Invalid enteredCourses data";
+        exit;
+    }
+ 
+    // Function to generate recommendations based on entered courses
+    function generateRecommendations($enteredCourses) {
+        // Send a POST request to the API endpoint to get available courses with no prerequisites
+        $apiUrl = "https://cis3760f23-13.socs.uoguelph.ca/site/rest-api/courses.php?action=available";
+        $postData = json_encode(["coursesTaken" => $enteredCourses]);
+ 
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'header' => 'Content-type: application/json',
+                'content' => $postData
+            ]
+        ];
+ 
+        $context = stream_context_create($options);
+        $response = file_get_contents($apiUrl, false, $context);
+ 
+        if ($response === false) {
+            throw new Exception("Failed to fetch recommended courses.");
+        }
+ 
+        $recommendations = json_decode($response, true);
+ 
+        if ($recommendations === null) {
+            throw new Exception("Error decoding recommended courses data.");
+        }
+ 
+        return $recommendations;
+    }
+ 
+    // Generate recommendations based on enteredCourses
+    $recommendations = generateRecommendations($enteredCourses);
+ 
+    // Create a response object
+    $response = [
+        "enteredCourses" => $enteredCourses,
+        "recommendedCourses" => $recommendations,
+    ];
+ 
+    // Send the response as JSON
+    echo json_encode($response);
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo "Method not allowed";
+    exit;
+}
+?>
--- sprint6/sprint6-files/html/course_generator/index.php
+++ sprint6/sprint6-files/html/course_generator/index.php
@@ -1,5 +1,4 @@
 <?php $pageTitle = "Course Generator Page"?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,11 +12,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Merriweather:wght@300&display=swap" rel="stylesheet">
-
+    
     <title><?php echo $pageTitle; ?></title>
 </head>
 <body>
-
   <div class="image-row">
       <div class="image-container">
           <img src="./books.png" alt="books">
@@ -27,75 +25,115 @@
           <img src="./books.png" alt="books">
       </div>
   </div>
-
+  
   <div class="home">
-      <a href=/><button class="btn-home"><i class="fa fa-home"></i></button></a>
+      <a href="/"><button class="btn-home"><i class="fa fa-home"></i></button></a>
   </div>
-
+  
   <div class="input-course-container">
       <h2>Step 1: Input Taken Courses</h2>
-      <p>Input the courses that you have taken below one at a time , clicking the “Add Course” button after each input.
-Note: The input should be in the format of SUBJECT*COURSE NUM,
-for example; CIS*2500.<p>
+      <p>Input the courses that you have taken below one at a time, clicking the “Add Course” button after each input.
+      Note: The input should be in the format of SUBJECT*COURSE NUM, for example; CIS*2500.</p>
       <div class="submit-course">
-          <form>
-              <input type="text" id="course" name="course" placeholder="Input Course..."><br>
-          </form>
+          <input type="text" id="course" name="course" placeholder="Input Course..."><br>
           <button class="btn1">Add Course</button>
       </div>
       <div class="enteredCoursesList">
-        <p>Courses inputted so far:</p>
+        <h3>Courses inputted so far:</h3>
+        <ul id="enteredCourses"></ul>
       </div>
   </div>
-
+ 
   <div class="generate-course-container">
       <h2>Step 2: Course Recommendations</h2>
       <p>Please confirm that all the courses you have taken are listed above. When ready, please click the button below to generate recomended courses.<p>
       <div class="generate-course">
           <button class="btn2">Generate Courses</button>
       </div>
-      <div class="courses-generated">
-      </div>
+    </div>
+    
+    <!-- div to display recommended courses -->
+    <div class="recommended-courses">
+        <h2>Recommended Courses:</h2>
+            <ul id="recommended-courses-list">
+                <!-- Recommended courses will be displayed here -->
+            </ul>
   </div>
-
+ 
   <div class="course-data-container">
-      <h2>Step 3: View Course Details</h2>
-      <p>Enter a course code in the format of SUBJECT*COURSE NUM in order to view its details.<p>
-      <div class="submit-course-detail">
-          <form>
-              <input type="text" id="course-detail" name="course-detail" placeholder="Input Course..."><br>
-          </form>
-          <button class="btn1">Search Course</button>
-      </div>
+    <h2>Step 3: View Course Details</h2>
+    <p>Enter a course code in the format of SUBJECT*COURSE NUM in order to view its details.</p>
+    <div class="submit-course-detail">
+      <input type="text" id="course-detail" name="course-detail" placeholder="Input Course..."><br>
+      <button class="btn3">Search Course</button>
+    </div>
   </div>
-
-</body>
-<script type="text/javascript">
-  document.addEventListener("DOMContentLoaded", function() {
+ 
+  <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
       let add_course_btn = document.querySelector(".btn1");
       let courseInput = document.getElementById("course");
-      let enteredCoursesList = document.querySelector(".enteredCoursesList");
-      let prereqs = [];
-
+      let enteredCoursesList = document.getElementById("enteredCourses");
+      let recommendedCoursesList = document.getElementById("recommendedCoursesList");
+ 
+      // Collect entered courses
+      let enteredCourses = [];
+ 
       add_course_btn.addEventListener("click", function() {
         let courseValue = courseInput.value;
-        
+ 
         let splitCourse = courseValue.split('*');
-      
-        // sanity check
+ 
         if (splitCourse.length === 2 && splitCourse[0] !== '' && splitCourse[1] !== '' && splitCourse[1].trim() !== '' && !isNaN(splitCourse[1])) {
-            prereqs.push(courseValue);
-            
-            let listItem = document.createElement("li");
-            listItem.textContent = courseValue;
-            enteredCoursesList.appendChild(listItem);
-            
-            courseInput.value = "";
+          enteredCourses.push(splitCourse[0] + "*" + splitCourse[1]); // Add to the list of entered courses
+ 
+          let listItem = document.createElement("li");
+          listItem.textContent = splitCourse[0] + "*" + splitCourse[1];
+          enteredCoursesList.appendChild(listItem);
+ 
+          courseInput.value = "";
         } else {
-            alert("Please enter the course in the format of SUBJECT*COURSE NUM, for example; CIS*2500");
+          alert("Please enter the course in the format of SUBJECT*COURSE NUM, for example; CIS*2500");
         }
       });
-    });
-</script>
-</html>
+ 
+// Generate Courses button click event
+function generateAndDisplayRecommendedCourses() {
+        let enteredCoursesInput = document.getElementById("enteredCourses");
+        enteredCoursesInput.value = JSON.stringify(enteredCourses);
 
+        // Make an AJAX request to generate_recommendations.php
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "generate_recommendations.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+
+                // Check if the response contains recommended courses
+                if (response.recommendedCourses) {
+                    response.recommendedCourses.sort((a, b) => {
+                      return a.courseCode.localeCompare(b.courseCode, undefined, { sensitivity: 'base' });
+                    });
+                    let recommendedCoursesList = document.getElementById("recommended-courses-list");
+                    recommendedCoursesList.innerHTML = ""; // Clear the list
+
+                    // Display only the course codes in the list
+                    response.recommendedCourses.forEach(function(course) {
+                        let courseCode = course.courseCode;
+                        let listItem = document.createElement("li");
+                        listItem.textContent = courseCode;
+                        recommendedCoursesList.appendChild(listItem);
+                    });
+                }
+            }
+        };
+        xhr.send("enteredCourses=" + JSON.stringify(enteredCourses));
+    }
+ 
+      let generateCourseBtn = document.querySelector(".btn2");
+      generateCourseBtn.addEventListener("click", generateAndDisplayRecommendedCourses);
+    });
+  </script>
+</body>
+</html>
