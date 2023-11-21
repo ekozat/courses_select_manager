2a39c861a8a12b58c8798178fdf1c5b2ac32b712
Maneesh Kandage Wijewardhana <mkandage@uoguelph.ca>
2023-11-21 16:36:27 -0500

    ok last test

--- sprint9/sprint9-files/html/index.php
+++ sprint9/sprint9-files/html/index.php
@@ -6,7 +6,7 @@ header('Content-Type: text/html; charset=UTF-8');
 $title = "F’23 CIS*3760: Group 101";
 $greeting = "Greetings and welcome to our digital realm!";
 $description1 = "We’re group 101! Although we’re small in size, we're mighty in effort!";
-$description2 = "Prepare to embark on a journey with us as we dive into Sprint 3. We look forward to showcasing our collective talents and pushing the boundaries of what's achievable in the digital realm. Welcome aboard!";
+$description2 = "Prepare to embark on a journey with us as we dive into our Software Engineering project. We look forward to showcasing our collective talents and pushing the boundaries of what's achievable in the digital realm. Welcome aboard!";
 
 $downloadFile = 'parsed_courses.xlsm';
 
